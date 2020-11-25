7df68796981fb0ac5074e485648f69a6c94e2193
harold <s.lefebvre907@laposte.net>
2020-11-25 16:41:41 +0100

    evaluation sql

--- eval.sql
+++ eval.sql
@@ -5,3 +5,7 @@ SELECT CompanyName,ContactName, ContactTitle, Phone From Customers where Country
 SELECT ProductName,UnitPrice FROM products INNER JOIN suppliers ON suppliers.SupplierID = products.SupplierID WHERE CompanyName = "Exotic Liquids"
 
 --3 - Nombre de produits vendus par les fournisseurs Français dans l’ordre décroissant
+select CompanyName as fournisseur, COUNT(suppliers.SupplierID) as nbr from products INNER join suppliers on suppliers.SupplierID = products.SupplierID where Country = "France" GROUP by suppliers.SupplierID order by nbr desc
+
+--4 - Liste des clients Français ayant plus de 10 commandes : pas fini 
+SELECT CompanyName AS client, COUNT(orders.CustomerID) AS Nbr From customers INNER join orders ON orders.CustomerID = customers.CustomerID where Country = "france"  GROUP by orders.CustomerID
